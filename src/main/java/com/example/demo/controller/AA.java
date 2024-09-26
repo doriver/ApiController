0acf6b214cc5cf50e39ca37a753948e8d5e4f477
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-09-26 16:23:48 +0900

    BindingResult를 추가해주면( 없을땐 400, bad request에러 발생 ) , size max조건만 어겼을때 > 정상 응답

--- src/main/java/com/example/demo/controller/AA.java
+++ src/main/java/com/example/demo/controller/AA.java
@@ -1,15 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class AA {
 
 	@RequestMapping("/aa")
-	public String aabb(@Validated @RequestBody Form01 form ) {
+	public String aabb(@Validated @RequestBody Form01 form, BindingResult br ) {
+		
+		if (br.hasErrors()) {
+			log.info("검증 오류 발생 {}",br);			
+		}
+		
 		return "1";
 	}
 }
